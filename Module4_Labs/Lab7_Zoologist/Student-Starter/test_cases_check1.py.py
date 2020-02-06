de1cf3405dd7f04764e1ae00bb8cf86675d1f166
Max Molchanov <mmolchanov@ucdavis.edu>
2020-02-05 22:41:05 -0800

    Fixes for 136

---
+++ Module4_Labs/Lab7_Zoologist/Student-Starter/test_cases_check1.py.py
@@ -0,0 +1,21 @@
+"""
+Test Case 1
+input: Animals.txt
+output: {0: 'Horse', 1: 'Buffalo', 2: 'Cat', 3: 'Rabbit', 4: 'Goat', 5: 'Dog', 6: 'Sheep', 7: 'Pig', 8: 'Cow', 9: 'Human', 10: 'Rat', 11: 'Mice', 12: 'Bat', 13: 'Squirrel', 14: 'Turtle'}
+"""
+
+"""
+Test Case 2
+input: print[equations]
+output: [1, 32, 8.0, 3, 0.0, 1.414213562373095, 0.1, 0.0, 6, 0.0, 1, 100.0, 1.0, 1, 1.414213562373095]
+"""
+
+"""
+Test Case 3
+input: on the start of the program
+output:
+Please chose a sorting method:
+1.(Bubble) Sort
+2.(Insertion) Sort
+3.(Selection) Sort
+"""
