549ac27dc55b0db3f007a738f0d9e1b8f9555546
Max Molchanov <mmolchanov@ucdavis.edu>
2020-02-05 23:00:35 -0800

    fixes

--- Module4_Labs/Lab7_Zoologist/Student-Starter/test_cases_check1.py.py
+++ Module4_Labs/Lab7_Zoologist/Student-Starter/test_cases_check1.py.py
@@ -11,8 +11,7 @@ output: [1, 32, 8.0, 3, 0.0, 1.414213562373095, 0.1, 0.0, 6, 0.0, 1, 100.0, 1.0,
 """
 
 """
-Test Case 3
-input: on the start of the program
+Test Case 3 (on the start of the program)
 output:
 Please chose a sorting method:
 1.(Bubble) Sort
