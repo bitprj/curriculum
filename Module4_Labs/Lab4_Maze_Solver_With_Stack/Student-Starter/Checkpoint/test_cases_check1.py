4e4a01f8ee0d7fa98596c9db35ee1003cc2d49ed
Sibbons Shrestha <sibbonsss@gmail.com>
2020-03-08 19:53:03 -0700

    made steps and checkpoint

---
+++ Module4_Labs/Lab4_Maze_Solver_With_Stack/Student-Starter/Checkpoint/test_cases_check1.py
@@ -0,0 +1,56 @@
+# For each test case a different map will be provided to ensure the algorithm works properly
+
+"""
+Test Case 1
+def main():
+    # load maze and another grid to display after exit found
+    mazeFile = "maze1.txt"
+    doneFile = "done.txt"
+    maze = loadFile(mazeFile)
+    done = loadFile(doneFile)
+    # print and solve maze
+    printGrid(maze)
+    solveMaze(maze)
+"""
+
+
+"""
+Test Case 2
+def main():
+    # load maze and another grid to display after exit found
+    mazeFile = "maze2.txt"
+    doneFile = "done.txt"
+    maze = loadFile(mazeFile)
+    done = loadFile(doneFile)
+    # print and solve maze
+    printGrid(maze)
+    solveMaze(maze)
+"""
+
+
+"""
+Test Case 3
+def main():
+    # load maze and another grid to display after exit found
+    mazeFile = "maze3.txt"
+    doneFile = "done.txt"
+    maze = loadFile(mazeFile)
+    done = loadFile(doneFile)
+    # print and solve maze
+    printGrid(maze)
+    solveMaze(maze)
+"""
+
+
+"""
+Test Case 4
+def main():
+    # load maze and another grid to display after exit found
+    mazeFile = "maze4.txt"
+    doneFile = "done.txt"
+    maze = loadFile(mazeFile)
+    done = loadFile(doneFile)
+    # print and solve maze
+    printGrid(maze)
+    solveMaze(maze)
+"""
