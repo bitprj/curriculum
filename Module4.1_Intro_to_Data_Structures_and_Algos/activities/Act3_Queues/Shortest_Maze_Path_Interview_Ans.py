0eda6d6f76f2492fa5b3d84fb35319cc6095f055
taimurkashif <taimurusca@gmail.com>
2020-03-25 16:47:44 -0700

    Create Interview Answer python file
    Created the file and started coding. gave credit to https://www.techiedelight.com/lee-algorithm-shortest-path-in-a-maze/ in comments

---
+++ Module4.1_Intro_to_Data_Structures_and_Algos/activities/Act3_Queues/Shortest_Maze_Path_Interview_Ans.py
@@ -0,0 +1,17 @@
+# Credit to https://www.techiedelight.com/lee-algorithm-shortest-path-in-a-maze/ for solution and problem.
+# queue node used in BFS
+class Node:
+	# (x, y) represents matrix cell coordinates
+	# dist represents its minimum distance from the source
+	def __init__(self, x, y, dist):
+		x = self.x
+		y = self.y
+		dist = self.dist
+
+
+
+
+
+		
+
+
